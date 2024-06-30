0545aef669f1ad5453942a69b6b32b46eaf020d1
Abhinav Nema <23abhinavnema@gmail.com>
2024-06-30 15:51:11 +0530

    Create Models for api

--- ItemList/api/models.py
+++ ItemList/api/models.py
@@ -1,3 +1,19 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Items(BaseModel):
+    key = models.CharField(max_length=50, null=False, blank=False)
+    value = models.TextField()
+
+    def __str__(self):
+        return self.key

ecef2ac620d27029bf78332cc920440b5248b144
Abhinav Nema <23abhinavnema@gmail.com>
2024-06-30 16:08:58 +0530

    Register Model to admin

--- ItemList/api/admin.py
+++ ItemList/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Items
 
-# Register your models here.
+
+@admin.register(Items)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'key', 'value', 'created_at', 'updated_at')
+    search_fields = ('key', 'value')

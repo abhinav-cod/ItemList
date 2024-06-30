fa751723bb8fd1d20eed0d6cbf474beb52f0290d
Abhinav Nema <23abhinavnema@gmail.com>
2024-06-30 15:52:59 +0530

    Create Views for query Model

--- ItemList/api/views.py
+++ ItemList/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Items
+from .serializers import ItemsSerializer
 
-# Create your views here.
+
+class ItemsViewSet(viewsets.ModelViewSet):
+    queryset = Items.objects.all()
+    serializer_class = ItemsSerializer

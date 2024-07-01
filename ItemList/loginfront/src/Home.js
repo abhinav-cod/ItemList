b2ecd82598f3d903f128665b3d27a4142826a3b5
Abhinav Nema <23abhinavnema@gmail.com>
2024-07-01 15:24:21 +0530

    Create Home page

---
+++ ItemList/loginfront/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to the Item List Project</h1>
+      <h4>Please Login using /login</h4>
+    </div>
+  );
+};
+
+export default Home;
